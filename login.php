bb6dbf0c7e2d780f48be6cbfd622573f897a037f
JarneSimons <r0901961@student.thomasmore.be>
2023-04-10 21:51:27 +0200

    style: adding style to loginscreen

--- login.php
+++ login.php
@@ -28,7 +28,7 @@
     <title>Dev4 - Log in</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-[#545454]">
+<body class="bg-[#545784]">
     <?php include_once(__DIR__ . "/nav.php"); ?>
 
     <div class="loginform">
