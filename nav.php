ff472535926e7b048c6db5bb68c6dcc956686298
Yorick <yorick.devleeschouwer@gmail.com>
2023-04-18 20:36:23 +0200

    update: did some php on navbar

--- nav.php
+++ nav.php
@@ -2,7 +2,10 @@
     <ul class="navbar">
         <li><h1>Eve</h1></li>
         <li><a href="index.php"><h1>Home</h1></a></li>
-        <li><a href="profile.php"><h1>Profile</h1></a></li>
+        <?php if(isset($_SESSION["loggedin"])):?>
+        <li><a href="profile.php"><h1><?php echo $_SESSION['username']; ?></h1></a></li>
+        <?php else:?>
         <li><a href="login.php"><h1>Log In</h1></a></li>
+        <?php endif;?>
     </ul>
 </nav>
