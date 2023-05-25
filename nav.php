6535eb7af2e9f567fbbdf1847c5481197e433e1d
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-05-25 17:25:07 +0200

    chore: Moderator page

--- nav.php
+++ nav.php
@@ -8,7 +8,7 @@
         <li><img src="<?php echo $url."evestore/assets/brand/od3krbvhegihsaahirrz.png"?>" alt="Eve" class="navlogo h-8 flex justify-start mr-8 ml-8 "></li>
         <li><a href="index.php"><h2 class="text-2xl mr-4 hover:text-[#0464A4]">Home</h2></a></li>
         <?php if(isset($_SESSION["admin"]) && $_SESSION["admin"] == true): ?>
-            <li><a href="adminpanel.php"><h2 class="text-2xl mr-4 hover:text-[#0464A4]">Admin panel</h2></a></li>
+            <li><a href="moderator.php"><h2 class="text-2xl mr-4 hover:text-[#0464A4]">Moderator</h2></a></li>
         <?php endif; ?>
         <?php if(isset($_SESSION["loggedin"])): ?>
             <li><a href="createprompt.php"><h2 class="text-2xl mr-4 hover:text-[#0464A4]">New prompt</h2></a></li>
