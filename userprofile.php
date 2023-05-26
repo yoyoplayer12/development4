62ec1114502c0e1586ad4e0627ac4b16002028a9
JarneSimons <r0901961@student.thomasmore.be>
2023-05-26 23:24:36 +0200

    feat: adding button to report user

--- userprofile.php
+++ userprofile.php
@@ -8,6 +8,7 @@
             header("Location: login.php");
         }
         $User = User::getUser($_GET['user']);
+        
         $prompts = [];
         $prompts = Prompt::getPromptsByUser($User["id"]);
 
@@ -29,8 +30,14 @@
 <body>
     <?php include_once(__DIR__ . "/nav.php"); ?>
 
+    <!-- <?php var_dump($_SESSION['id']); ?> -->
+    <!-- <?php var_dump($User['id']); ?> -->
+
     <?php if(isset($_SESSION['loggedin'])):?>
         <h1><?php echo $User["username"]?>'s profile</h1>
+        
+        <button class="reportUserbtn" id="reportUserbtnid" data-userId="<?php echo $User["id"]?>"><?php if(count(User::checkReportUser($User['id'])) >=1 ){ echo "Reported";} else { echo "Report";} ?></button>
+        
         <p class="profile-bio">Bio: <?php echo $User["bio"];?></p>
         <img src="<?php echo $url.$User["avatar_url"] ?>" alt="Avatar" class="rounded-full w-40 h-40 object-cover">
 
@@ -48,3 +55,35 @@
     <?php endif;?>
 </body>
 </html>
+
+<script>
+    let reportUser = document.querySelectorAll("#reportUserbtnid");
+    reportUser.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+            console.log("test");
+            let currentBtn = this;
+            let userId = this.dataset.userid;
+            //post naar database
+            let formData = new FormData();
+            formData.append("reported_id", userId);
+            
+            fetch("ajax/reportUser.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(json){
+                if (json.status == 'success') {
+                    currentBtn.innerHTML = json.message;
+                    console.log(json.message);
+                }
+
+            });
+
+        });
+    });
+
+
+</script>
