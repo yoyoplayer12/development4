0d8981374801be4eabe61a22c341aafdea015b0e
Yorick Devleeschouwer <64488176+yoyoplayer12@users.noreply.github.com>
2023-05-27 16:06:36 +0200

    fix: editprofile

--- editprofile.php
+++ editprofile.php
@@ -6,7 +6,6 @@
         $getUser = User::getSessionUser();
         if(!empty($_POST)){
             if(!empty($_POST['bio']) && !empty($_FILES['avatar_url']["name"])){
-    
                 if (strlen($_POST['bio']) <= 19){
                     $warning = "Bio is too short, has to have at least 20 characters.";
                 }
@@ -47,11 +46,6 @@
             }
             elseif(empty($_POST['bio']) && !empty($_FILES['avatar_url']["name"])){
                 $user = new User();
-            if (strlen($_POST['bio']) <= 19) {
-                $warning = "Bio is too short, has to have at least 20 characters";
-            } elseif (strlen($_POST['bio']) >= 501) {
-                $warning = "Bio is too long, can have a maximum of 500 characters";
-            } else {
                 $bio = $_POST['bio'];
                 $user = new User();
                 $user->resetBio($bio);
@@ -67,36 +61,37 @@
                 $user->updateAvatar();
                 $user->updateUser();
                 header("Location: profile.php");
-            }
-        } elseif (!empty($_POST['bio']) && empty($_FILES['avatar_url']["name"])) {
-            if (strlen($_POST['bio']) <= 19) {
-                $warning = "Bio is too short, has to have at least 20 characters";
-            } elseif (strlen($_POST['bio']) >= 501) {
-                $warning = "Bio is too long, can have a maximum of 500 characters";
-            } else {
-                $bio = $_POST['bio'];
+            } 
+            elseif (!empty($_POST['bio']) && empty($_FILES['avatar_url']["name"])) {
+                if (strlen($_POST['bio']) <= 19) {
+                    $warning = "Bio is too short, has to have at least 20 characters";
+                } elseif (strlen($_POST['bio']) >= 501) {
+                    $warning = "Bio is too long, can have a maximum of 500 characters";
+                } else {
+                    $bio = $_POST['bio'];
+                    $user = new User();
+                    $user->resetBio($bio);
+                    $user->updateBio();
+                    header("Location: profile.php");
+                }
+            }   
+            elseif (empty($_POST['bio']) && !empty($_FILES['avatar_url']["name"])) {
                 $user = new User();
-                $user->resetBio($bio);
-                $user->updateBio();
+                //set image
+                $upload = new Image();
+                $upload->setup();
+                $upload->upload("public", "users", "avatar_url");
+                $randomstring = $upload->getString();
+                $ext = pathinfo($_FILES['avatar_url']['name'], PATHINFO_EXTENSION);
+                $destination = "evestore/public/users/" . $randomstring . "." . $ext;
+                $user->resetAvatar($destination);
+                //saving to database
+                $user->updateAvatar();
                 header("Location: profile.php");
+            } 
+            else {
+                echo "nothing to save";
             }
-        } elseif (empty($_POST['bio']) && !empty($_FILES['avatar_url']["name"])) {
-            $user = new User();
-
-            //set image
-            $upload = new Image();
-            $upload->setup();
-            $upload->upload("public", "users", "avatar_url");
-            $randomstring = $upload->getString();
-            $ext = pathinfo($_FILES['avatar_url']['name'], PATHINFO_EXTENSION);
-            $destination = "evestore/public/users/" . $randomstring . "." . $ext;
-            $user->resetAvatar($destination);
-            //saving to database
-
-            $user->updateAvatar();
-            header("Location: profile.php");
-        } else {
-            echo "nothing to save";
         }
     }
     else {
@@ -105,17 +100,9 @@
     //setting up image getting
     $image = new Image();
     $url = $image->getUrl()
-} else {
-    header("Location: login.php");
-}
-
-//setting up image getting
-$image = new Image();
-$url = $image->getUrl()
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -127,7 +114,6 @@ $url = $image->getUrl()
     <title>Eve - <?php echo $_SESSION['username']?>'s profile</title>
     <title>Eve - <?php echo $_SESSION['username'] ?>'s profile</title>
 </head>
-
 <body>
     <?php include_once(__DIR__ . "/nav.php"); ?>
     <div class="flex justify-center items-center mt-10">
@@ -136,12 +122,10 @@ $url = $image->getUrl()
                 <div class=" w-90 rounded-3xl bg-white px-10 py-10 ">
                     <h1 class="text-2xl font-bold text-center"><?php echo $_SESSION['username'] ?>'s profile</h1>
                     <form action="" method="post" enctype="multipart/form-data" class="flex justify-center flex-col">
-                        
                         <div class="flex justify-center flex-col items-center mb-5">
                             <label for="avatar_url" class="text-xl font-bold text-center">Avatar</label>
                             <img src="<?php echo $url . $getUser["avatar_url"] ?>" alt="Avatar" class="rounded-full w-40 h-40 object-cover my-5">
                             <input type="file" id="user-reset-avatar" name="avatar_url" accept="image/*">
-                            
                         </div>
                         <div class="flex justify-center flex-col items-center gap-2 mb-5">
                             <label for="bio">Bio</label>
@@ -151,13 +135,11 @@ $url = $image->getUrl()
                         <p class="editprofilewarning"><?php echo $warning ?></p>
                         <input type="submit" value="Save" class="mx-2 px-4 py-2 rounded-lg bg-blue-500 hover:bg-blue-600 text-white cursor-pointer">
                     </form>
-
-                <?php else : ?>
-                    <?php header("Location: ./profile.php"); ?>
-                <?php endif; ?>
                 </div>
+            <?php else : ?>
+                <?php header("Location: ./profile.php"); ?>
+            <?php endif; ?>  
         </div>
     </div>
 </body>
-
 </html>
