1ca96c5fae4139e16498da2624df67269cf82bfd
JarneSimons <r0901961@student.thomasmore.be>
2023-05-28 13:07:00 +0200

    style: formatting files

--- nav.php
+++ nav.php
@@ -13,7 +13,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
         </button>
-        <ul id="menu" class="hidden md:flex md:items-center md:gap-5 md:bg-transparent md:py-4 md:px-8 absolute top-16 left-0 w-full z-10 md:static md:w-auto md:ml-0 <?php echo (isset($_SESSION["loggedin"]) ? 'bg-black' : ''); ?>">
+        <ul id="menu" class="hidden md:flex md:items-center md:gap-5 md:bg-transparent md:py-4 md:px-8 absolute top-16 left-0 w-full z-10 md:static md:w-auto md:ml-0 <?php echo(isset($_SESSION["loggedin"]) ? 'bg-black' : ''); ?>">
             <li><a href="index.php" class="block py-2 text-2xl text-white hover:text-[#0464A4]">Home</a></li>
             <?php if (isset($_SESSION["admin"]) && $_SESSION["admin"] == true) : ?>
                 <li><a href="moderator.php" class="block py-2 text-2xl text-white hover:text-[#0464A4]">Moderator</a></li>
@@ -21,7 +21,8 @@
             <?php if (isset($_SESSION["loggedin"])) : ?>
                 <li><a href="createprompt.php" class="block py-2 text-2xl text-white hover:text-[#0464A4]">New prompt</a></li>
                 <li><a href="profile.php" class="block py-2 text-2xl text-white hover:text-[#0464A4]"><?php echo htmlspecialchars($_SESSION['username']); ?></a></li>
-                <li class="text-blue-500 text-2xl"><?php $balance = User::getBalance(); echo $balance; ?> Credits</li>
+                <li class="text-blue-500 text-2xl"><?php $balance = User::getBalance();
+                echo $balance; ?> Credits</li>
             <?php else : ?>
                 <li><a href="login.php" class="block py-2 text-2xl text-white hover:text-[#0464A4]">Log In</a></li>
                 <li><a href="register.php" class="block py-2 text-2xl text-white hover:text-[#0464A4]">Register</a></li>
