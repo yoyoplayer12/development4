f93194b11184fd2bba1d33e55725c2704d6ec54b
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-05-28 04:06:43 +0200

    style: login responsive

--- login.php
+++ login.php
@@ -41,26 +41,32 @@ $url = $image->getUrl();
 <body class="bg-blue-200">
     <?php include_once(__DIR__ . "/nav.php"); ?>
 
-    <div class="form flex flex-row">
-        <div class="w-1/2 h-100 flex justify-center items-center">
-                <form action="" method="post" >
-                    <img src="<?php echo $url . "evestore/assets/brand/od3krbvhegihsaahirrz.png" ?>" class="flex justify-center w-50 ml-10 mb-10">
-                    <h1 class="mb-15 text-[#0464A4] text-5xl mb-10">Log in</h1>
-                    <ul>
-                        <li><input class="border-2 flex w-full justify-center rounded-md mb-5 py-2" type="text" name="username" placeholder="Username" required></li>
-                        <li><input class="border-2 flex w-full justify-center rounded-md mb-15 py-2" type="password" name="password" placeholder="Password" required></li>
-                        <li class="text-[#FF0000] mb-5"><?php echo $loginwarning ?></li>
-                        <li><input type="submit" value="Log in" class="flex w-full justify-center mb-5 rounded-md bg-[#0464A4] hover:bg-[#0444A4] py-3 text-sm font-semibold text-white cursor-pointer"></li>
-                        <li class="mb-5 text-[#0464A4]"><a href="forgotpassword.php">Forgot password?</a></li>
-                        <li>Don't have an account? <a href="register.php" class="text-[#0464A4]">Register</a></li>
-                    </ul>
-    
-                </form>
-        </div>
-        <div class="w-1/2">
-            <img src="<?php echo $url . "evestore/assets/images/nz01zmtboksuyko7cmam.jpg" ?>" class="w-full h-screen">
-        </div>
+    <div class="form flex flex-col sm:flex-row">
+    <div class="w-full sm:w-1/2 h-screen flex justify-center items-center">
+        <form action="" method="post">
+            <img src="<?php echo $url . 'evestore/assets/brand/od3krbvhegihsaahirrz.png' ?>" class="sm:block w-50 ml-10 mb-10">
+            <h1 class="mb-15 text-[#0464A4] text-5xl mb-10 text-center">Log in</h1>
+            <ul class="flex flex-col items-center">
+                <li><input class="border-2 px-4 py-2 rounded-md mb-5 text-base" type="text" name="username" placeholder="Username" required></li>
+                <li><input class="border-2 px-4 py-2 rounded-md mb-15 text-base" type="password" name="password" placeholder="Password" required></li>
+                <li class="text-[#FF0000] mb-5"><?php echo $loginwarning ?></li>
+                <li><input type="submit" value="Log in" class=" px-8 py-3 rounded-md bg-[#0464A4] hover:bg-[#0444A4] font-semibold text-white cursor-pointer mb-5"></li>
+                <li class="mb-5 text-[#0464A4]"><a href="forgotpassword.php">Forgot password?</a></li>
+                <li>Don't have an account? <a href="register.php" class="text-[#0464A4]">Register</a></li>
+            </ul>
+        </form>
     </div>
+    <div class="hidden sm:block w-full sm:w-1/2">
+        <img src="<?php echo $url . 'evestore/assets/images/nz01zmtboksuyko7cmam.jpg' ?>" class="w-full h-screen">
+    </div>
+</div>
+
+
+
+
+
+
+
 
 </body>
 
